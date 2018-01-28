72bc0176edd1d84f2ce491777b1c70a22e439cb3
Andrey <longot@gmail.com>
2018-01-28 12:17:11 +0200

    v

---
+++ Assets/Scripts/AddTransformerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTransformerScript : MonoBehaviour {
+
+    int activeStep = 3;
+    bool isExecuted;
+    bool isExecuted2;
+    FigureScript figure;
+    GameManagerScript gameManager;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
+        figure = GameObject.Find("FigureGroup").GetComponent<FigureScript>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isExecuted && activeStep == gameManager.GetStep())
+        {
+            isExecuted = true;
+            figure.AddElement(gameManager.GetStep());
+        }
+        if (!isExecuted2 && gameManager.GetStep() == 5)
+        {
+            isExecuted2 = true;
+            figure.AddElement(gameManager.GetStep());
+        }
+    }
+
+    public void SetActiveStep(int step)
+    {
+        activeStep = step;
+    }
+}
---
+++ Assets/Scripts/Stabilizer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stabilizer : MonoBehaviour {
+    GameObject[] objects;
+    public int upperGroundCounter;
+    public int lowerGroundCounter;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        objects = GameObject.FindGameObjectsWithTag("Block");
+        foreach (GameObject block in objects)
+        {
+            var coordinates = block.GetComponent<ElementScript>();
+            if(coordinates.localY != 5)
+            {
+                upperGroundCounter++;
+            }
+            if(coordinates.localY < 5)
+            {
+                lowerGroundCounter++;
+                break;
+            }
+        }
+        if (lowerGroundCounter > 0)
+        {
+            foreach (GameObject block in objects)
+            {
+                var coordinates = block.GetComponent<ElementScript>();
+                coordinates.localY++;
+            }
+        }
+        if (upperGroundCounter == objects.Length)
+        {
+            foreach (GameObject block in objects)
+            {
+                var coordinates = block.GetComponent<ElementScript>();
+                coordinates.localY--;
+            }
+        }
+        upperGroundCounter = 0;
+        lowerGroundCounter = 0;
+    }
+}
+
+//ЗАРАБОТАЕТ ПОСЛЕ ТОГО, КАК НАСТРОИМ ТРАНСФОРМ ПО ЛОКАЛХ И ЛОКАЛУ
